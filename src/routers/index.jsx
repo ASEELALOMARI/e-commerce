39c10a4718c5b4f1840df23fb4b337c8c0ee453b
ASEELALOMARI <392206377@qu.edu.sa>
2024-11-14 02:23:19 +0300

    Fix manage product

--- src/routers/index.jsx
+++ src/routers/index.jsx
@@ -11,7 +11,6 @@ import ProtectedRoute from "./ProtectedRoute";
 import AdminProtectedRoute from "./AdminProtectedRoute";
 import NotAuthorized from "../components/responses/NotAuthorized";
 import ProductDetails from "../components/products/ProductDetails";
-import ManageProduct from "../components/admin/manegeProduct/ManegeProduct";
 import CreateNewProduct from "../components/admin/manegeProduct/CreateNewProduct";
 import UpdateProduct from "../components/admin/manegeProduct/UpdateProduct";
 import ManegeCategories from "../components/admin/manegeCategories/ManegeCategories";
@@ -30,6 +29,7 @@ import ManageOrders from "../components/admin/manegeOrders/ManegeOrders";
 import UserOrders from "../components/UserOrder/UserOrders";
 import NotFound from "../components/responses/NotFound";
 import Example from "../components/form/example";
+import ManageProduct from "../components/admin/manegeProduct/ManegeProduct";
 
 const PropertyWrapper = () => (
   <CartProvider>
